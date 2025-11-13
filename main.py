4499f49bf1b190959a533f2618cf84fa1806704f
ajamol2197 <ajamol2197@gmail.com>
2025-11-13 17:32:27 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,83 @@
+shahar = "kokon"
+viloyat = "Fargona"
+
+matn = "men yangi 📱 oldim"
+print(matn)
+
+ism = "ahmad"
+print("mening ismim " + ism)
+
+ism = "ahad"
+familya = ' qayum'
+print(ism + familya)
+
+ism = 'Ahad'
+familiya = 'Qayum'
+print(ism + ' ' + familiya)
+
+ism  = "ahad"
+familya = "qayum"
+ism_sharif = f"{ism} {familya}"
+print(ism_sharif)
+
+ism = "james"
+familya = "bond"
+print(f" salom mening ismim {familya}. {ism} {familya}")
+
+print("hello world")
+print("hello \tworld ")
+print("hello \nworld")
+
+ism = "ahad"
+familya = "qayum"
+ism_sharif = f"{ism} {familya}"
+print(ism_sharif.upper())
+
+ism = "Ahad"
+familya = "Qayum"
+ism_sharif = f"{ism} {familya}"
+print(ism_sharif.lower())
+
+ism = "james bond"
+print(ism.title())
+
+ism = "james bond"
+print(ism.capitalize())
+
+print('james bond'.upper())
+
+
+meva = "    olma     "
+print("men" + meva.lstrip() + "yaxshi koraman")
+print("men" + meva.rstrip() + "yaxshi koraman")
+print("men" + meva.strip() + "yaxshi koraman")
+print("men" + meva + "yaxshi koraman")
+
+ism = input('isminggiz nima?')
+print("assalomu aleykum, " + ism)
+
+ism = input("isminggiz niam/>>>>")
+print("assalomu aleykum," + ism.title())
+
+
+
+kocha="Bog'bon"
+mahalla="Sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+
+print(f"{kocha} kochasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+
+kocha = input("Ko'changiz: ")
+mahalla = input("Mahallangiz: ")
+tuman = input("Tumaningiz: ")
+viloyat = input("Viloyatingiz: ")
+
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil)
+
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
